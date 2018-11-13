9192d022d29f071e239784b63a62995ef96ecff8
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-11-14 02:03:34 +0800

    remove my channel section from left side menu

--- app/src/app/containers/DrawerLeft/index.tsx
+++ app/src/app/containers/DrawerLeft/index.tsx
@@ -34,39 +34,10 @@ class DrawerLeft extends React.Component<Props, {}> {
     );
   }
 
-  renderMyChannels() {
-    let list = [1, 2, 3, 4, 5];
-    return (
-      <div>
-        {list.map((elem, i) => {
-          return <div key={i} className="each-channel-container">
-            <div className="each-drawer-icon-container">
-              <FaSquare />
-            </div>
-            <div className="each-drawer-title-container">
-              My Channel {i}
-            </div>
-
-          </div>;
-        })}
-      </div>
-    );
-  }
-
-  renderLineBreak() {
-    return (
-      <div>
-        <hr />
-      </div>
-    );
-  }
-
   render() {
     return (
       <div className="drawer-left-container">
         {this.renderDomains()}
-        {this.renderLineBreak()}
-        {this.renderMyChannels()}
       </div>
     );
   }
