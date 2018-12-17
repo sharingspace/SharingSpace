ca679eaed851965040e5bd5ee15d40056f28acf8
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-12-17 20:27:46 +0800

    update members store with new api

--- app/src/app/stores/Domain/MemberStore.js
+++ app/src/app/stores/Domain/MemberStore.js
@@ -7,14 +7,22 @@ class MembersStore {
     this.fetchingData = false;
   }
 
+  async asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+
   async getToken() {
     const response = await axios({
       method: 'post',
       url: 'https://app.sharing.space/oauth/token',
       data: {
-        grant_type: 'client_credentials',
-        client_id: 2,
-        client_secret: 'oyEZ2fZK48SPQizdWyIc5kPupYbxy8trumTMCerw'
+        grant_type: 'password',
+        client_id: '3',
+        client_secret: '7q17kc3DonY0d2nlVCytFkrmFqI9bJL5GPRo2k4t',
+        username: 'dhaval48@gmail.com',
+        password: '123456'
       }
     })
     return response.data.access_token
@@ -33,16 +41,16 @@ class MembersStore {
     const existingToken = localStorage.getItem(tokenName)
     if (existingToken) { // if a token exists, check expiration
       try {
-        const members = await this.getMembers(existingToken) // try to query members
-        if (members.data.length) {
+        const communities = await this.getCommunities(existingToken) // try to query communities
+        if (communities.length) {
           console.log('[!] token not expired, returning')
           return existingToken
         } else {
-          console.log('[!] token might be expired inside if, regenerate', e.message)
+          console.log('[! inside if] token might be expired, regenerate. error:', e.message)
           return await this.generateToken()
         }
       } catch(e) {
-        console.log('[!] caught e: token might be expired, regenerate', e.message)
+        console.log('[! caught e] token might be expired, regenerate. error:', e.message)
         return await this.generateToken()
       }
     } else {
@@ -51,18 +59,38 @@ class MembersStore {
     }
   }
 
-  async getMembers(token) {
+  async getCommunities(token) {
     try {
-      const url = 'https://app.sharing.space/api/v1/members?per_page=10'
+      const url = 'https://app.sharing.space/api/v1/allcommunities'
       const response = await axios({
-        method: 'post',
+        headers: {
+          Authorization: token,
+          Accept: 'application/json'
+        },
+        method: 'get',
+        url
+      })
+      return response.data
+    } catch (e) {
+      console.log('[!] getcommunities error', e.message)
+      throw e
+    }
+  }
+
+  async getMembers(communityId, token) {
+    try {
+      const url = `https://app.sharing.space/api/v1/members/${communityId}`
+      const response = await axios({
+        method: 'get',
         url,
         headers: {
-          Authorization: token
+          Authorization: token,
+          Accept: 'application/json'
         }
       })
       return response.data;
     } catch (e) {
+      console.log('[!] getmembers error', e.message)
       throw e
     }
   }
@@ -71,12 +99,22 @@ class MembersStore {
   async fetchMembers() {
     this.fetchingData = true;
     const token = await this.checkToken()
-    const members = await this.getMembers(token)
+    const communities = await this.getCommunities(token) // format: [ true, "", [{ id, name, ... }]]
 
-    runInAction('Update state after fetching members', () => {
-      this.members = toJS(members);
-      this.fetchingData = false;
-    });
+    const combineMembers = async () => {
+      let members = []
+      await this.asyncForEach(communities[2], async (community) => { // see communities format for `communities[2]` explanation
+        const communityMembers = await this.getMembers(community.id, token)
+        members = members.concat(communityMembers[2])
+      });
+
+      runInAction('Update state after fetching members', () => {
+        this.members = toJS(members);
+        this.fetchingData = false;
+      });
+    }
+    
+    combineMembers();
   };
 }
 
